5d919d3d95ee6dd640281a02e05c3d07e2de6506
Nouran <nouran.mhmoud@gmail.com>
2018-02-02 12:23:28 +0100

    number-abbreviation and approximate-number settings

--- main.js
+++ main.js
@@ -25,6 +25,12 @@
   const formatInput = $('.format');
   const formatBox = $('.format-box');
   const localeBox = $('.locale-box');
+  const approximateNumberSettings = $('.approximate-number-settings');
+  const min10k = $('.min10k');
+  const capital = $('.capital');
+  const round = $('.round');
+  const numbeAbbreviateSettings = $('.number-abbreviate-settings');
+  const abbrFormat = $('.abbr-format');
 
   //output elements
   const output = $('.output');
@@ -54,25 +60,25 @@
 
       case 'number-abbreviate':
         let numAbbr = new NumAbbr(['k', 'm', 'b', 't']);
-        formatedNum = numAbbr.abbreviate(num, 2);
+        formatedNum = numAbbr.abbreviate(num, Number(abbrFormat.val()));
         rawValue = num;
         break;
 
       case 'approximate-number':
         formatedNum = approx(num, {
           decimal: ',',
-          min10k: true,
+          min10k: min10k.is(':checked'),
           prefix: '$',
-          capital: true,
-          round: true,
+          capital: capital.is(':checked'),
+          round: round.val(),
         });
+
         rawValue = num;
         break;
 
       case 'numbro':
         formatedNum = numbro(num).format(format);
         rawValuem = numbro(formatedNum).value();
-        debugger;
         break;
 
       default:
@@ -96,6 +102,10 @@
   library.on('change', onChange);
   input.on('keyup', onChange);
   formatInput.on('keyup', onChange);
+  min10k.on('change', onChange);
+  capital.on('change', onChange);
+  round.on('change', onChange);
+  abbrFormat.on('keyup', onChange);
 
   library.on('change', function libraryChange(e){
     prosCons.show().not('.'+library.val()).hide();
@@ -105,6 +115,18 @@
     } else {
       formatBox.hide();
       localeBox.hide();
+    }
+
+    if (library.val() === 'approximate-number') {
+      approximateNumberSettings.show();
+    } else {
+      approximateNumberSettings.hide();
+    }
+
+    if (library.val() === 'number-abbreviate') {
+      numbeAbbreviateSettings.show();
+    } else {
+      numbeAbbreviateSettings.hide();
     }
 
     return libraryChange;
